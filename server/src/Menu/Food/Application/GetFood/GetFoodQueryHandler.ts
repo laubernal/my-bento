8a3f56419f36f78caa07911c73a0d3b110c2c855
laubernal <lbernalrodrguez@gmail.com>
2024-08-04 11:53:38 +0200

    add logs to get food use case

--- server/src/Menu/Food/Application/GetFood/GetFoodQueryHandler.ts
+++ server/src/Menu/Food/Application/GetFood/GetFoodQueryHandler.ts
@@ -7,23 +7,31 @@ import { FoodFilter } from '../../Domain/Filter/FoodFilter';
 import { GetFoodResponse } from './GetFoodResponse';
 import { RecordNotFoundError } from 'Shared/Domain/Error/RecordNotFoundError';
 import { Id } from 'Shared/Domain/Vo/Id.vo';
+import { MyBentoLogger } from 'Shared/Infrastructure/Logger/MyBentoLogger';
 
 @QueryHandler(GetFoodQuery)
 export class GetFoodQueryHandler implements IQueryHandler<GetFoodQuery> {
-  constructor(@Inject('IFoodRepository') private readonly repository: IFoodRepository) {}
+  constructor(
+    @Inject('IFoodRepository') private readonly repository: IFoodRepository,
+    private readonly logger: MyBentoLogger
+  ) {}
 
   public async execute(query: GetFoodQuery): Promise<GetFoodResponse> {
     const id = new Id(query.id);
 
-    const food = await this.findFood(id);
+    const food = await this.findFood(id, query.traceId);
 
     return GetFoodResponse.toResponse(food);
   }
 
-  private async findFood(id: Id): Promise<Food> {
+  private async findFood(id: Id, traceId: string): Promise<Food> {
     const filter = FoodFilter.create().withId(id);
 
+    this.logger.log('Before querying DB to get food', [traceId]);
+
     const result = await this.repository.findOne(filter);
+
+    this.logger.log('After querying DB to get food', [traceId]);
 
     if (typeof result === 'undefined') {
       throw new RecordNotFoundError();
--- server/src/Menu/Food/Infrastructure/Controllers/GetFood/GetFoodController.ts
+++ server/src/Menu/Food/Infrastructure/Controllers/GetFood/GetFoodController.ts
@@ -1,22 +1,32 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Headers, Param, Res } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { Response } from 'express';
 import { GetFoodQuery } from 'src/Menu/Food/Application/GetFood/GetFoodQuery';
 import { GetFoodParams } from './GetFoodParams';
+import { MyBentoLogger } from 'Shared/Infrastructure/Logger/MyBentoLogger';
 
 @Controller()
 export class GetFoodController {
-  constructor(private readonly queryBus: QueryBus) {}
+  constructor(private readonly queryBus: QueryBus, private readonly logger: MyBentoLogger) {}
 
   @Get('api/foods/:id')
-  public async get(@Param() params: GetFoodParams, @Res() res: Response) {
+  public async get(
+    @Param() params: GetFoodParams,
+    @Headers('traceId') traceId: string,
+    @Res() res: Response
+  ) {
     try {
-      const query = GetFoodQuery.fromJson(params);
+      this.logger.log('Starting to get food', [traceId]);
+
+      const query = GetFoodQuery.fromJson(params, traceId);
 
       const response = await this.queryBus.execute(query);
 
+      this.logger.log('Sending found food', [traceId]);
       return res.status(200).json(response);
     } catch (error: any) {
+      this.logger.error(`Error getting food: ${error.message}`, [traceId]);
+
       return res.status(400).json({ error: error.message });
     }
   }
