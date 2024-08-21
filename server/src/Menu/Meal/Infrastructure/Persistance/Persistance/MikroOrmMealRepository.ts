5731628b7050de2d7fb9aac99efed1d48a29723f
laubernal <lbernalrodrguez@gmail.com>
2024-08-21 11:20:11 +0200

    implement meal repository methods

--- server/src/Menu/Meal/Infrastructure/Persistance/Persistance/MikroOrmMealRepository.ts
+++ server/src/Menu/Meal/Infrastructure/Persistance/Persistance/MikroOrmMealRepository.ts
@@ -4,6 +4,9 @@ import { IMealRepository } from 'Menu/Meal/Domain/Repository/IMealRepository';
 import { MealMapper } from '../Mapper/MealMapper';
 import { MealFilter } from 'Menu/Meal/Domain/Filter/MealFilter';
 import { Meal } from 'Menu/Meal/Domain/Entity/Meal';
+import { MikroOrmMealFilterAdapter } from '../File/MikroOrmMealFilterAdapter';
+import { MealEntity } from 'Shared/Infrastructure/Persistance/Model/MealEntityMikroOrm';
+import { MealFoodEntity } from 'Shared/Infrastructure/Persistance/Model/MealFoodEntityMikroOrm';
 
 @Injectable()
 export class MikroOrmMealRepository implements IMealRepository {
@@ -11,7 +14,16 @@ export class MikroOrmMealRepository implements IMealRepository {
 
   public async findOne(filter: MealFilter): Promise<Meal | undefined> {
     try {
-      throw new Error('Method not implemented');
+      const adapter = new MikroOrmMealFilterAdapter(filter);
+      const adapterQuery = adapter.apply();
+
+      const result = await this.entityManager.findOne(MealEntity, adapterQuery);
+
+      if (!result) {
+        return undefined;
+      }
+
+      return this.mapper.toDomain(result);
     } catch (error: any) {
       throw new Error(`Meal Repository Error -- ${error}`);
     }
@@ -19,7 +31,14 @@ export class MikroOrmMealRepository implements IMealRepository {
 
   public async find(filter: MealFilter): Promise<Meal[]> {
     try {
-      throw new Error('Method not implemented');
+      const adapter = new MikroOrmMealFilterAdapter(filter);
+      const adapterQuery = adapter.apply();
+
+      const result = await this.entityManager.findAll(MealEntity, adapterQuery);
+
+      return result.map((meal: MealEntity) => {
+        return this.mapper.toDomain(meal);
+      });
     } catch (error: any) {
       throw new Error(`Meal Repository Error -- ${error}`);
     }
@@ -27,7 +46,17 @@ export class MikroOrmMealRepository implements IMealRepository {
 
   public async save(entity: Meal): Promise<void> {
     try {
-      throw new Error('Method not implemented');
+      const newEntity = this.mapper.toModel(entity);
+
+      for (const mealFood of newEntity.foods) {
+        const food = this.entityManager.create(MealFoodEntity, mealFood);
+
+        await this.entityManager.persistAndFlush(food);
+      }
+
+      const meal = this.entityManager.create(MealEntity, newEntity);
+
+      await this.entityManager.persistAndFlush(meal);
     } catch (error: any) {
       throw new Error(`Meal Repository Error -- ${error}`);
     }
@@ -35,7 +64,18 @@ export class MikroOrmMealRepository implements IMealRepository {
 
   public async update(entity: Meal): Promise<void> {
     try {
-      throw new Error('Method not implemented');
+      const filter = MealFilter.create().withId(entity.id());
+
+      const adapter = new MikroOrmMealFilterAdapter(filter);
+      const adapterQuery = adapter.apply();
+
+      const newMeal = this.mapper.toModel(entity);
+
+      for (const mealFood of newMeal.foods) {
+        await this.entityManager.nativeUpdate(MealFoodEntity, { id: mealFood.id }, mealFood);
+      }
+
+      this.entityManager.nativeUpdate(MealEntity, adapterQuery, newMeal);
     } catch (error: any) {
       throw new Error(`Meal Repository Error -- ${error}`);
     }
@@ -43,7 +83,16 @@ export class MikroOrmMealRepository implements IMealRepository {
 
   public async delete(entity: Meal): Promise<void> {
     try {
-      throw new Error('Method not implemented');
+      const filter = MealFilter.create().withId(entity.id());
+
+      const adapter = new MikroOrmMealFilterAdapter(filter);
+      const adapterQuery = adapter.apply();
+
+      for (const mealFood of entity.foods()) {
+        await this.entityManager.nativeDelete(MealFoodEntity, { id: mealFood.id().value });
+      }
+
+      await this.entityManager.nativeDelete(MealEntity, adapterQuery);
     } catch (error: any) {
       throw new Error(`Meal Repository Error -- ${error}`);
     }
