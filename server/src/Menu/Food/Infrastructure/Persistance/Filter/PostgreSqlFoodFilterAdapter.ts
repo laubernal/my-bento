423a35965f6d1d01e2190eb3a9259c0e5f34dedf
laubernal <lbernalrodrguez@gmail.com>
2024-09-07 17:37:07 +0200

    implement postgre sql filter adapters

---
+++ server/src/Menu/Food/Infrastructure/Persistance/Filter/PostgreSqlFoodFilterAdapter.ts
@@ -0,0 +1,51 @@
+import { FoodFilter } from 'Menu/Food/Domain/Filter/FoodFilter';
+import { Id } from 'Shared/Domain/Vo/Id.vo';
+import { Name } from 'Shared/Domain/Vo/Name.vo';
+import { PostgreSqlAdapter } from 'Shared/Infrastructure/Persistance/Adapter/PostgreSqlAdapter';
+
+export class PostgreSqlFoodFilterAdapter extends PostgreSqlAdapter {
+  constructor(private filter: FoodFilter) {
+    super();
+  }
+
+  //   public apply(): any {
+  //     const filters = this.filter.apply();
+
+  //     if (filters.has(FoodFilter.FOOD_NAME_FILTER)) {
+  //       const name = filters.get(FoodFilter.FOOD_NAME_FILTER) as Name;
+
+  //       this.assign({ name: name.value });
+  //     }
+
+  //     if (filters.has(FoodFilter.FOOD_ID_FILTER)) {
+  //       const id = filters.get(FoodFilter.FOOD_ID_FILTER) as Id;
+
+  //       this.assign({ id: id.value });
+  //     }
+
+  //     return this.applyOrderBy(filters);
+  //   }
+
+  public apply(): string {
+    const filters = this.filter.apply();
+    let conditions = [];
+
+    if (filters.has(FoodFilter.FOOD_NAME_FILTER)) {
+      const name = filters.get(FoodFilter.FOOD_NAME_FILTER) as Name;
+
+      conditions.push(`name = '${name.value}'`);
+    }
+
+    if (filters.has(FoodFilter.FOOD_ID_FILTER)) {
+      const id = filters.get(FoodFilter.FOOD_ID_FILTER) as Id;
+      
+      conditions.push(`id = '${id.value}'`);
+    }
+
+    const whereClause = conditions.length ? `WHERE ${conditions.join(' AND ')}` : '';
+
+    const orderByClause = this.applyOrderBy(filters);
+
+    return `${whereClause} ${orderByClause}`.trim();
+  }
+}
