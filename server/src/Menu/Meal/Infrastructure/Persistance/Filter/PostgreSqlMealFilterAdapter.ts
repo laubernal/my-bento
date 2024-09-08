313d85071660dc6662702537cbc6e409df240062
laubernal <lbernalrodrguez@gmail.com>
2024-09-08 09:58:13 +0200

    implement postgresql meal filter adapter

---
+++ server/src/Menu/Meal/Infrastructure/Persistance/Filter/PostgreSqlMealFilterAdapter.ts
@@ -0,0 +1,33 @@
+import { MealFilter } from 'Menu/Meal/Domain/Filter/MealFilter';
+import { Id } from 'Shared/Domain/Vo/Id.vo';
+import { Name } from 'Shared/Domain/Vo/Name.vo';
+import { PostgreSqlAdapter } from 'Shared/Infrastructure/Persistance/Adapter/PostgreSqlAdapter';
+
+export class PostgreSqlMealFilterAdapter extends PostgreSqlAdapter {
+  constructor(private filter: MealFilter) {
+    super();
+  }
+
+  public apply(): any {
+    const filters = this.filter.apply();
+    const conditions = [];
+
+    if (filters.has(MealFilter.MEAL_NAME_FILTER)) {
+      const name = filters.get(MealFilter.MEAL_NAME_FILTER) as Name;
+
+      conditions.push(`name = ${name.value}`);
+    }
+
+    if (filters.has(MealFilter.MEAL_ID_FILTER)) {
+      const id = filters.get(MealFilter.MEAL_ID_FILTER) as Id;
+
+      conditions.push(`id = ${id.value}`);
+    }
+
+    const whereClause = conditions.length ? `WHERE ${conditions.join(' AND ')}` : '';
+
+    const orderByClause = this.applyOrderBy(filters);
+
+    return `${whereClause} ${orderByClause}`.trim();
+  }
+}
