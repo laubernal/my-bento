e38c9e355b34b03a0121305483219bd02d0aaefd
laubernal <lbernalrodrguez@gmail.com>
2025-09-06 12:02:35 +0200

    feat: add filter by array of ids

--- server/src/Menu/Food/Domain/Filter/FoodFilter.ts
+++ server/src/Menu/Food/Domain/Filter/FoodFilter.ts
@@ -1,28 +1,34 @@
-import { Filter } from 'Shared/Domain/Entities/Filter';
-import { Id } from 'Shared/Domain/Vo/Id.vo';
-import { Name } from 'Shared/Domain/Vo/Name.vo';
+import {Filter} from 'Shared/Domain/Entities/Filter';
+import {Id} from 'Shared/Domain/Vo/Id.vo';
+import {Name} from 'Shared/Domain/Vo/Name.vo';
 
 export class FoodFilter extends Filter {
-  public static FOOD_NAME_FILTER = 'food';
-  public static FOOD_ID_FILTER = 'id';
+    public static FOOD_NAME_FILTER = 'food';
+    public static FOOD_ID_FILTER = 'id';
+    public static FOOD_IDS_FILTER = 'ids';
 
-  public static create(): FoodFilter {
-    return new FoodFilter();
-  }
+    protected data: Map<string, any> = new Map();
 
-  protected data: Map<string, any> = new Map();
+    public static create(): FoodFilter {
+        return new FoodFilter();
+    }
 
-  public withName(name: Name): this {
-    this.data.set(FoodFilter.FOOD_NAME_FILTER, name);
-    return this;
-  }
+    public withName(name: Name): this {
+        this.data.set(FoodFilter.FOOD_NAME_FILTER, name);
+        return this;
+    }
 
-  public withId(id: Id): this {
-    this.data.set(FoodFilter.FOOD_ID_FILTER, id);
-    return this;
-  }
+    public withId(id: Id): this {
+        this.data.set(FoodFilter.FOOD_ID_FILTER, id);
+        return this;
+    }
 
-  public apply(): Map<string, any> {
-    return this.data;
-  }
+    public withIds(ids: Id[]): this {
+        this.data.set(FoodFilter.FOOD_IDS_FILTER, ids);
+        return this;
+    }
+
+    public apply(): Map<string, any> {
+        return this.data;
+    }
 }
--- server/src/Menu/Food/Infrastructure/Persistance/Filter/PostgreSqlFoodFilterAdapter.ts
+++ server/src/Menu/Food/Infrastructure/Persistance/Filter/PostgreSqlFoodFilterAdapter.ts
@@ -11,6 +11,7 @@ export class PostgreSqlFoodFilterAdapter extends PostgreSqlAdapter {
   public apply(): string {
     const filters = this.filter.apply();
     let conditions = [];
+    let inClause = [];
 
     if (filters.has(FoodFilter.FOOD_NAME_FILTER)) {
       const name = filters.get(FoodFilter.FOOD_NAME_FILTER) as Name;
@@ -21,10 +22,18 @@ export class PostgreSqlFoodFilterAdapter extends PostgreSqlAdapter {
     if (filters.has(FoodFilter.FOOD_ID_FILTER)) {
       const id = filters.get(FoodFilter.FOOD_ID_FILTER) as Id;
 
-      conditions.push(`id = '${id.value}'`);
+      conditions.push(`id  '${id.value}'`);
     }
 
-    const whereClause = conditions.length ? `WHERE ${conditions.join(' AND ')}` : '';
+    if (filters.has(FoodFilter.FOOD_IDS_FILTER)) {
+      const ids = filters.get(FoodFilter.FOOD_IDS_FILTER) as Id[];
+
+      const formattedIds = ids.map(id => `'${id.value}'`);
+
+      inClause.push(`id IN (${formattedIds})`);
+    }
+
+    const whereClause = conditions.length || inClause.length ? `WHERE ${conditions.join(' AND ')} ${inClause.join(' ')}` : '';
 
     const orderByClause = this.applyOrderBy(filters);
 
