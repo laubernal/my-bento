e0469176a3d09c2c50bf0e401bedfa45a65ecff2
laubernal <lbernalrodrguez@gmail.com>
2025-10-05 11:54:14 +0200

    refactor: move file to persistance folder

--- server/src/Menu/Menu/Infrastructure/Persistance/Persistance/PostgreSqlMenuRepository.ts
+++ server/src/Menu/Menu/Infrastructure/Persistance/Persistance/PostgreSqlMenuRepository.ts
@@ -2,9 +2,17 @@ import {Injectable} from '@nestjs/common';
 import {Menu} from 'Menu/Menu/Domain/Entity/Menu';
 import {IMenuRepository} from 'Menu/Menu/Domain/Repository/IMenuRepository';
 import {Filter} from 'Shared/Domain/Entities/Filter';
+import {PostgreSqlDatabaseService} from 'Shared/Infrastructure/Persistance/PostgreSql/PostgreSqlDatabase';
+import {PostgreSqlMenuMapper} from 'Menu/Menu/Infrastructure/Persistance/Mapper/PostgreSqlMenuMapper';
 
 @Injectable()
 export class PostgreSqlMenuRepository implements IMenuRepository {
+    constructor(
+        private readonly mapper: PostgreSqlMenuMapper,
+        private readonly databaseService: PostgreSqlDatabaseService
+    ) {
+    }
+    
     public async findOne(filter: Filter): Promise<Menu | undefined> {
         throw new Error('Method not implemented.');
     }
@@ -14,6 +22,21 @@ export class PostgreSqlMenuRepository implements IMenuRepository {
     }
     
     public async save(entity: Menu): Promise<void> {
+        try {
+            const {meals, ...menu} = this.mapper.toModel(entity);
+            
+            await this.databaseService.query('BEGIN;');
+            
+            // todo: IMPLEMENT QUERY TO SAVE MENU
+            
+            await this.databaseService.query('COMMIT;');
+        } catch (error: any) {
+            await this.databaseService.query('ROLLBACK;');
+            
+            throw new Error(`Menu Repository Error -- ${error}`);
+        }
+        
+        
         throw new Error('Method not implemented.');
     }
     
