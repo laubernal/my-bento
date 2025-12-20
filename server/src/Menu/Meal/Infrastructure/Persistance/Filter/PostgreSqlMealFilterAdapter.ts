a0cd54edfdc4248065469d403107c5eb44ff6313
laubernal <lbernalrodrguez@gmail.com>
2025-12-20 18:30:55 +0100

    feat: add with ids filter to meal filter

--- server/src/Menu/Meal/Infrastructure/Persistance/Filter/PostgreSqlMealFilterAdapter.ts
+++ server/src/Menu/Meal/Infrastructure/Persistance/Filter/PostgreSqlMealFilterAdapter.ts
@@ -8,34 +8,44 @@ export class PostgreSqlMealFilterAdapter extends PostgreSqlAdapter {
     constructor(private filter: MealFilter) {
         super();
     }
-
+    
     public apply(): any {
         const filters = this.filter.apply();
         const conditions = [];
-
+        let inClause = [];
+        
         if (filters.has(MealFilter.MEAL_NAME_FILTER)) {
             const name = filters.get(MealFilter.MEAL_NAME_FILTER) as Name;
-
+            
             conditions.push(`meals.name ILIKE '%${name.value}%'`);
         }
-
+        
         if (filters.has(MealFilter.MEAL_TYPE_FILTER)) {
             const type = filters.get(MealFilter.MEAL_TYPE_FILTER) as MealType;
-
+            
             conditions.push(`meals.type = '${type.value}'`);
         }
-
+        
         if (filters.has(MealFilter.MEAL_ID_FILTER)) {
             const id = filters.get(MealFilter.MEAL_ID_FILTER) as Id;
-
+            
             conditions.push(`meals.id = '${id.value}'`);
         }
-
-        const whereClause = conditions.length ? `WHERE ${conditions.join(' AND ')}` : '';
-
+        
+        if (filters.has(MealFilter.MEAL_IDS_FILTER)) {
+            const ids = filters.get(MealFilter.MEAL_IDS_FILTER) as Id[];
+            
+            const formattedIds = ids.map(id => `'${id.value}'`);
+            
+            inClause.push(`id IN (${formattedIds})`);
+        }
+        
+        const whereClause = conditions.length || inClause.length ?
+            `WHERE ${conditions.join(' AND ')} ${inClause.join(' ')}` : '';
+        
         const orderByClause = this.applyOrderBy(filters);
         const paginationClause = this.applyPagination(filters);
-
+        
         return `${whereClause} ${orderByClause} ${paginationClause}`.trim();
     }
 }
