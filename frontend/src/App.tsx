c3e7db39ee228678f2ef561caf151ef17b798148
laubernal <lbernalrodrguez@gmail.com>
2026-09-05 16:50:43 +0200

    feat: add custom theme to mantine

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import '@mantine/core/styles.css';
-import {MantineProvider} from '@mantine/core';
+import {colorsTuple, createTheme, MantineProvider} from '@mantine/core';
 import {AppMenu} from './components/appMenu.tsx';
+import {ACCENT_COLORS, BRAND_COLORS, WARM_COLORS} from './utils.ts';
+
+const theme = createTheme({
+    fontFamily: 'Montserrat, Open Sans, sans-serif',
+    primaryColor: 'brand',
+    colors: {
+        brand: colorsTuple(BRAND_COLORS),
+        warm: colorsTuple(WARM_COLORS),
+        accent: colorsTuple(ACCENT_COLORS),
+    },
+    defaultRadius: '10px',
+});
 
 function App() {
     return (
-        <MantineProvider>
-            <>
-                <AppMenu />
-            </>
+        <MantineProvider theme={theme}>
+            <AppMenu/>
         </MantineProvider>
     );
     
---
+++ frontend/src/utils.ts
@@ -0,0 +1,38 @@
+export const BRAND_COLORS: string[] = [
+    '#FFFCF7',
+    '#e8a882',
+    '#d4845e',
+    '#C4673A',
+    '#a85530',
+    '#8c4426',
+    '#70351c',
+    '#542612',
+    '#381808',
+    '#1c0c04'
+]
+
+export const WARM_COLORS: string[] = [
+    '#F8F4EE',
+    '#F3EDE4',
+    '#F0EAE0',
+    '#EDE6DA',
+    '#E0D5C5',
+    '#8B7D6E',
+    '#6B5D4E',
+    '#4A3728',
+    '#3A2F1E',
+    '#2A1F14'
+]
+
+export const ACCENT_COLORS: string[] = [
+    '#FFFCF7',
+    '#d6e0d4',
+    '#b3c4b0',
+    '#8B9E88',
+    '#6e8168',
+    '#516448',
+    '#344728',
+    '#172a08',
+    '#0b1504',
+    '#050a02'
+]
