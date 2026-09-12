7355268815c687c30ddfe0d6f10d05eea3f88111
laubernal <lbernalrodrguez@gmail.com>
2026-09-12 17:38:14 +0200

    feat: import outfit font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import {AppMenu} from './components/appMenu.tsx';
 import {ACCENT_COLORS, BRAND_COLORS, WARM_COLORS} from './utils.ts';
 
 const theme = createTheme({
-    fontFamily: 'Fraunces, Montserrat, Open Sans, sans-serif',
+    fontFamily: 'Outfit, Fraunces, Montserrat, Open Sans, sans-serif',
     primaryColor: 'brand',
     colors: {
         brand: colorsTuple(BRAND_COLORS),
