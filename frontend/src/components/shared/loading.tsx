6f76cb6a55e2fc80ce7aa7760f237af40af9ca0d
laubernal <lbernalrodrguez@gmail.com>
2026-09-12 16:34:05 +0200

    feat: use custom theme color

--- frontend/src/components/shared/loading.tsx
+++ frontend/src/components/shared/loading.tsx
@@ -1,23 +1,23 @@
-import {Center, Loader} from '@mantine/core';
+import {Center, Loader, useMantineTheme} from '@mantine/core';
 
 interface Props {
     loading: boolean;
 }
 
 export function Loading({loading}: Props) {
+    const theme = useMantineTheme();
     return (
         <>
             {loading ?
                 <Center>
                     <Loader
-                        color="teal"
+                        color={theme.colors.accent[7]}
                         type="dots"
                         size={50}
                     />
                 </Center>
                 : <></>
             }
-        
         </>
     
     );
